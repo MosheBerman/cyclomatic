9fc159300eec8209033dfbb8bae508f03fed08c3
Moshe Berman <moshberm@gmail.com>
2012-11-29 23:58:40 -0500

    Initial Commit

---
+++ Cyclomat/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Cyclomat
+//
+//  Created by Moshe Berman on 11/29/12.
+//  Copyright (c) 2012 Moshe Berman. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
